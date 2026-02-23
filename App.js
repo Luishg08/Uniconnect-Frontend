9cded57b98c8e5b612588942d10bbcaadedad856
Luis Henao <lhenao@consensussa.com>
2026-02-23 10:03:52 -0500

    feat: first commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { Button, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId: "",
+  });
+
+  useEffect(() => {
+    if (response) {
+    console.log("FULL RESPONSE:");
+    console.log(JSON.stringify(response, null, 2));
+  }
+  if (request) {
+    console.log("FULL REQUEST:");
+    console.log(JSON.stringify(request, null, 2));
+  }
+  }, [response]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 100 }}>
+      <Button
+        title="Login con Google"
+        disabled={!request}
+        onPress={() => promptAsync()}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
